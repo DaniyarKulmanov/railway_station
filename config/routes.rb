93c7fefdb23f92f4e242c5fad35687c165c596a6
daniyarkulmanov@gmail.com <scorpion>
2017-09-25 10:27:20 +0300

    create model Route and routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  resources :trains
-  resources :railway_stations
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trains, :railway_stations, :route
 end
