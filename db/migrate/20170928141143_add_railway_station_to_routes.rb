a9f9119c40783a1755007182d80f000df92bf188
sindry <DanRubyDeveloper@gmail.com>
2017-10-01 16:10:36 +0600

    Fix route <=> trains

--- db/migrate/20170928141143_add_railway_station_to_routes.rb
+++ db/migrate/20170928141143_add_railway_station_to_routes.rb
@@ -1,5 +1,4 @@
 class AddRailwayStationToRoutes < ActiveRecord::Migration[5.1]
   def change
-    add_belongs_to :trains, :current_station
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170930153058) do
+ActiveRecord::Schema.define(version: 20171001100712) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20170930153058) do
     t.datetime "updated_at", null: false
     t.integer "current_station_id"
     t.integer "ticket_id"
+    t.integer "route_id"
+    t.index ["route_id"], name: "index_trains_on_route_id"
   end
 
   create_table "users", force: :cascade do |t|
