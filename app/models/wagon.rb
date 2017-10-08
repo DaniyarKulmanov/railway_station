581c97ee91ebf13e4dbf5799509f4b5365103f2e
sindry <DanRubyDeveloper@gmail.com>
2017-10-08 17:42:15 +0600

    validation

--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -1,6 +1,7 @@
 class Wagon < ApplicationRecord
+  WAGON_TYPE = ['Купе', 'Плацкарт'].freeze
+
   belongs_to :train
 
-  # validates :comfort_level, format: /Купе|Плацкарт/
-  # WAGON_TYPE = ['Купе', 'Плацкарт'].freeze
+  validates :comfort_level, inclusion: { in: WAGON_TYPE }
 end
