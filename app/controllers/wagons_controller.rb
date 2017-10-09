72b106c53ab0715df7e6bc17e47c29601b9fecd7
sindry <DanRubyDeveloper@gmail.com>
2017-10-09 09:27:21 +0600

    wagon_controller permit :comfort_level

--- app/controllers/wagons_controller.rb
+++ app/controllers/wagons_controller.rb
@@ -45,6 +45,6 @@ class WagonsController < ApplicationController
     end
 
     def wagon_params
-      params.require(:wagon).permit(:upper_seats, :bottom_seats, :train_id)
+      params.require(:wagon).permit(:comfort_level, :upper_seats, :bottom_seats, :train_id)
     end
 end
