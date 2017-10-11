8404f0d646e37a6fb54552ac54b41061960d4bda
sindry <DanRubyDeveloper@gmail.com>
2017-10-11 21:21:30 +0600

    Added Station_number in routes

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -4,4 +4,9 @@ class RailwayStation < ApplicationRecord
   has_many :routes, through: :railway_stations_routes
 
   validates :title, presence: true
+
+  def update_station_number(route, number)
+    route_stations = railway_stations_routes.where(route: route).first
+    route_stations.update(station_number: number) if route_stations
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006063445) do
+ActiveRecord::Schema.define(version: 20171011144506) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171006063445) do
   create_table "railway_stations_routes", force: :cascade do |t|
     t.integer "railway_station_id"
     t.integer "route_id"
+    t.integer "station_number"
   end
 
   create_table "routes", force: :cascade do |t|
