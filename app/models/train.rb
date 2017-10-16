4d04fc5bd9793930cc917b732d2ae69fd3e41944
sindry <DanRubyDeveloper@gmail.com>
2017-10-16 17:01:44 +0600

    last task

--- app/models/train.rb
+++ app/models/train.rb
@@ -15,6 +15,9 @@ class Train < ApplicationRecord
      self.wagons.each do |wagon|
        count += wagon.upper_seats if wagon.comfort_level == type && seat == :upper
        count += wagon.bottom_seats if wagon.comfort_level == type && seat == :bottom
+       count += wagon.side_top_seats if wagon.comfort_level == type && seat == :side_top
+       count += wagon.side_bottom_seats if wagon.comfort_level == type && seat == :side_bottom
+       count += wagon.sitting_seats if wagon.comfort_level == type && seat == :sitting
      end
      count
   end
