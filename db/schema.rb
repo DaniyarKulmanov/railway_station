26dcbfb6247afa437dc33fae6ea07019df373dd0
sindry <DanRubyDeveloper@gmail.com>
2017-10-25 08:33:23 +0600

    Arrival and departure datetime

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018140219) do
+ActiveRecord::Schema.define(version: 20171025022831) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20171018140219) do
     t.integer "railway_station_id"
     t.integer "route_id"
     t.integer "station_number"
+    t.datetime "arrival"
+    t.datetime "departure"
   end
 
   create_table "routes", force: :cascade do |t|
