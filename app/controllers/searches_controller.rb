4b788930504904d577fa7d834129c2c7d70fc955
sindry <DanRubyDeveloper@gmail.com>
2017-10-29 17:26:28 +0600

    Search part 1

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,4 +3,14 @@ class SearchesController < ApplicationController
   def show
   end
 
+  def new
+    @trains = Search.trains(params[:station_from], params[:station_to])
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:station_from, :station_to)
+  end
+
 end
