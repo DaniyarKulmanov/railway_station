50faad7fc345db28c939942b0f93200f186dfb9b
sindry <DanRubyDeveloper@gmail.com>
2017-11-13 16:33:54 +0600

    Fixed params values

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,20 @@
 class SearchesController < ApplicationController
+  before_action :search_trains, only: :show
 
   def show
   end
 
   def new
-    @trains = Search.trains(params[:station_from], params[:station_to])
+    @railway_stations = RailwayStation.all
   end
 
   private
 
+  def search_trains
+    @trains = Search.trains(params[:search][:station_from], params[:search][:station_to])
+  end
+
   def search_params
     params.require(:search).permit(:station_from, :station_to)
   end
-
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,15 +1,15 @@
 class Search
-  @trains = []
   def self.trains(station_from, station_to)
-    routes_from = Route.joins(:railway_stations).where('railway_stations.id': station_from)
-    routes_to = Route.joins(:railway_stations).where('railway_stations.id': station_to)
+    trains = []
+    routes_from = Route.joins(:railway_stations).where('railway_stations.id':station_from)
+    routes_to = Route.joins(:railway_stations).where('railway_stations.id':station_to)
     routes_from.each do |route_from|
       routes_to.where(id: route_from).each do |route|
         route.trains.each do |train|
-          @trains << train
+          trains << train
         end
       end
     end
-    @trains
+    trains
   end
 end
