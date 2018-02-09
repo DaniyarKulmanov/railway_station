0d767fb2af04043bec442224f2118a2d09371bf0
sindry <DanRubyDeveloper@gmail.com>
2018-02-09 19:56:58 +0600

    ticket params

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -8,7 +8,25 @@ class TicketsController < ApplicationController
   end
 
   def create
-    render plain: params[:ticket].inspect
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      redirect_to @ticket
+    else
+      render :new
+    end
   end
 
+  private
+
+  def ticket_params
+    params.require(:ticket).permit(
+                                    :full_name,
+                                    :passport_number,
+                                    :train_id,
+                                    :user_id,
+                                    :to_station_id,
+                                    :from_station_id
+                                  )
+  end
 end
