2dca00a9741fd194b8e3b67a540f7697c72a8484
sindry <DanRubyDeveloper@gmail.com>
2018-02-12 15:50:30 +0600

    Link to tickets from search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,6 +1,6 @@
 class Search
 
-  def self.routes(station_from, station_to)
+  def self.routes(station_from, station_to) # TODO fix routes array, throwing in the end in view of search/show
     routes = []
     routes_from = Route.joins(:railway_stations).where('railway_stations.id':station_from)
     routes_to = Route.joins(:railway_stations).where('railway_stations.id':station_to)
