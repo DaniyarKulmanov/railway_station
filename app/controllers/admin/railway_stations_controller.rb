c91631e80a732e8258ea4570041f89d45367f09f
sindry <DanRubyDeveloper@gmail.com>
2018-03-12 21:59:23 +0600

    fix routes views and controllers

--- app/controllers/admin/railway_stations_controller.rb
+++ app/controllers/admin/railway_stations_controller.rb
@@ -19,7 +19,7 @@ class Admin::RailwayStationsController < Admin::BaseController
     @railway_station = RailwayStation.new(railway_station_params)
 
     if @railway_station.save
-      redirect_to @railway_station
+      redirect_to [:admin, @railway_station]
     else
       render :new
     end
@@ -27,7 +27,7 @@ class Admin::RailwayStationsController < Admin::BaseController
 
   def update
     if @railway_station.update(railway_station_params)
-      redirect_to @railway_station
+      redirect_to [:admin, @railway_station]
     else
       render :edit
     end
@@ -41,7 +41,7 @@ class Admin::RailwayStationsController < Admin::BaseController
   def update_position
     @route = Route.find(params[:route_id])
     @railway_station.update_station_number(@route, params[:station_number])
-    redirect_to @route
+    redirect_to [:admin, @route]
   end
 
   private
